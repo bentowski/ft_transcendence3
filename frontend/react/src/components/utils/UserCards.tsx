1083669b024636d243b19ae69cce774912a13f6b
bentowski <benjamin.baudry@hotmail.com>
2022-10-01 16:31:43 +0200

    push intermediaire

--- frontend/react/src/components/utils/UserCards.tsx
+++ frontend/react/src/components/utils/UserCards.tsx
@@ -1,82 +1,72 @@
 import { Component } from 'react';
 
-class UserCards extends Component<{value:number, avatar:boolean}, {}> {
+class UserCards extends Component<{value:number, avatar:boolean}, {onload: number}> {
 	state = {
 		onload: 0
 	}
 
 
-		renderUserCards(id: number) {
-			async function test(url: any)
-			{
-				const settings = {
-					method: 'GET',
-					body: null
-				}
-				const response: any = await fetch(url, settings)
-				if (response.ok)
-				{
-					return (await response.value);
-				}
-				return;
+		renderUserCards = async (id: number) => {
+			const settings = {
+				method: 'GET',
 			}
-			const url = "http://localhost:3000/user/" + id;
-			let wtf: Promise<any> = test(url);
-			console.log(wtf);
-			let login: string = "la";
-			if (this.props.avatar)
+			const response: any = await fetch("http://localhost:3000/user/" + id, settings)
+			if (response.ok)
 			{
+				let login = await response.json().username;
+				if (this.props.avatar)
+				{
+					return (
+						<div key={id} className="friendsDiv row my-2">
+							<div className="col-3">
+								<button>Tchat</button>
+								<button>Play</button>
+							</div>
+							<div className="col-3">
+								<img src="online"/>
+							</div>
+							<div className="col-6 row">
+								<p className="col-6">{login}</p>
+								<img src="avatar" className="col-6"/>
+							</div>
+						</div>
+					)
+				}
 				return (
-					<div key={id} className="friendsDiv row my-2">
+					<div key={login} className="friendsDiv row my-2">
 						<div className="col-3">
-							<button>Tchat</button>
+							<button>Chat</button>
 							<button>Play</button>
 						</div>
 						<div className="col-3">
 							<img src="online"/>
 						</div>
 						<div className="col-6 row">
-							<p className="col-6">{login}</p>
-							<img src="avatar" className="col-6"/>
+							<p className="col-12">{login}</p>
 						</div>
 					</div>
 				)
 			}
-			return (
-				<div key={login} className="friendsDiv row my-2">
-					<div className="col-3">
-						<button>Chat</button>
-						<button>Play</button>
-					</div>
-					<div className="col-3">
-						<img src="online"/>
-					</div>
-					<div className="col-6 row">
-						<p className="col-12">{login}</p>
-					</div>
-				</div>
-			)
-			// console.log(login);
 		}
 
-	onloadFct = () => {
+	onloadFct = async () => {
 		let ret:any = [];
 
-		this.state.onload = 1;
-		let x = 0; //variable a changer selon le back
-		// const ret:any = []
+		let x = 0;
 		while(x < 2) {
-			ret.push(this.renderUserCards(x + 1))
+			ret.push(await this.renderUserCards(x + 1))
 			x++;
 		}
+		console.log("ret : " + ret) //unexpected return
 		return ret;
 	}
 
 	render() {
 		let items:any = [];
 		if (this.state.onload == 0)
-			items = this.onloadFct();
-
+		{
+			this.onloadFct();
+		}
 		return (
 			<div>
 				{items}
