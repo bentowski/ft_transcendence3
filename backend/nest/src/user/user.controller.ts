9acf2a927908094763ff0077bfef8f642e5369cc
LuluLink <sokol.lucas@gmail.com>
2022-10-06 13:06:11 +0200

    upload fonctionnel

--- backend/nest/src/user/user.controller.ts
+++ backend/nest/src/user/user.controller.ts
@@ -5,12 +5,31 @@ import {
   Get,
   Param,
   Post,
+  UploadedFile,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ValidateCreateUserPipe } from './pipes/validate-create-user.pipe';
+import { Observable, of } from 'rxjs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer'
+import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
+
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads/profileimages',
+    filename: (req:any, file:any, cb:any) => {
+      let filename: string = file.originalname.replace(/\s/g, '');// + uuidv4();
+      let lastDot = filename.lastIndexOf('.');
+      filename = filename.substring(0, lastDot) + uuidv4() + filename.substring(lastDot);
+      cb(null, `${filename}`)
+    }
+  })
+}
 
 @Controller('user')
 export class UserController {
@@ -39,5 +58,12 @@ export class UserController {
   @Delete()
   remove(@Param('id') username: string) {
     return this.userService.remove(username);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', storage))
+  uploadFile(@UploadedFile() file:any): Observable<Object> {
+    console.log(file);
+    return of({imagePath: file.filename});
   }
 }
