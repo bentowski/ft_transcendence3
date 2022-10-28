9b978b391b680593823501bda5a7576eaa407385
toto l'astico <toto@lasti.co>
2022-10-28 13:29:48 +0200

    ok

--- frontend/react/src/pages/Login.tsx
+++ frontend/react/src/pages/Login.tsx
@@ -38,7 +38,7 @@ class Login extends Component<any, any> {
           <h2 className="d-flex flex-row justify-content-center align-items-center">Sign up with your </h2>
           <h2 className="d-flex flex-row justify-content-center align-items-center"><img className="Logo mx-2 pt-1" src="./logos/42-logo.svg"></img> account</h2>
         </div>
-        <a href="https://api.intra.42.fr/oauth/authorize?client_id=0ca73eb0dd76ab61dabb62b46c3a31885e924d813db06a480056b2080f9b0126&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fredirect&response_type=code">
+        <a href="http://localhost:3000/auth/login">
           <button className="mb-2 mx-2">Sign in</button>
         </a>
       </div>
