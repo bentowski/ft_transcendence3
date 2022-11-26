0529a1baf253529c98a87d76233359b4853cf12c
bentowski <37326350+bentowski@users.noreply.github.com>
2022-11-26 20:19:18 +0100

    Delete frontend/react/src/hooks directory

--- frontend/react/src/hooks/useCurrentUser.tsx
+++
@@ -1,5 +0,0 @@
-export const useCurrentUser = () => {
-  //const { user } = useAuthData();
-  //return user;
-};
-export default useCurrentUser;
--- frontend/react/src/hooks/useTwoFa.tsx
+++
@@ -1,9 +0,0 @@
-import {useAuthData} from "../contexts/AuthProviderContext";
-/*
-const useTwoFa = () => {
-    const { isTwoFa } = useAuthData();
-
-    return (isTwoFa);
-}
-export default useTwoFa;
-*/
