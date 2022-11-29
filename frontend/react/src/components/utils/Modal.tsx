7b94408227ee6bacacc6efb091b84deef475013e
ivloisy <ivloisy@student.42.fr>
2022-11-29 15:18:47 +0100

    fix hidden join chan + link avatar

--- frontend/react/src/components/utils/Modal.tsx
+++ frontend/react/src/components/utils/Modal.tsx
@@ -484,6 +484,16 @@ class Modal extends Component<
 
   joinRoom = (newRoom: ChanType) => {
     this.props.parentCallBack.joinRoom(newRoom)
+    // console.log(join)
+    // this.hiddenJoin()
+  }
+
+  joinRoomPublic = (key: number) => {
+    this.props.parentCallBack.joinRoom(
+      this.state.allChans[key],
+      true
+    )
+    this.hiddenJoin()
   }
 
   chans = async () => {
@@ -506,10 +516,7 @@ class Modal extends Component<
                     :
                     <button
                       onClick={() =>
-                        this.props.parentCallBack.joinRoom(
-                          this.state.allChans[x],
-                          true
-                        )
+                        this.joinRoomPublic(x)
                       }
                     >
                       JOIN
