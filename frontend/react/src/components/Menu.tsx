a689475269b3bdbbef856524b8cb6b171089134d
ivloisy <ivloisy@student.42.fr>
2022-11-29 18:21:29 +0100

    css

--- frontend/react/src/components/Menu.tsx
+++ frontend/react/src/components/Menu.tsx
@@ -41,18 +41,24 @@ const Menu = () => {
     <div className="Menu shadow d-flex justify-content-between align-items-center">
       <div className="homeButtonDiv col-3 d-flex justify-content-start">
         <Link to={"/chat"}>
-          <p className="m-0">chat</p>
+          <div className="m-0">chat</div>
         </Link>
         <Link to={"/history"}>
-          <p className="m-0 mx-2">stats</p>
+          <div className="m-0 mx-2">stats</div>
         </Link>
       </div>{" "}
       {/* homeButtonDiv */}
-      <div className="titleDiv">
+      <div className="titleDiv d-none d-sm-block">
         <h1 className="m-0"><img alt="" className="BobLePong" src="http://localhost:8080/pictures/boblepongelogo.png"/></h1>
       </div>{" "}
       {/* titleDiv */}
       <div className="profilMenu d-flex justify-content-end align-items-center col-3">
+        <div className="logoutMenu">
+          {/* <a className="mx-2 btn btn-outline-dark shadow-none" onClick={logoutSession}> */}
+          <a className="mx-2" onClick={logoutSession}>
+            <p className="m-0">logout</p>
+          </a>
+        </div>
         <div className="loginMenu px-2">
           <Link to={"/profil/" + username}>
             <p className="m-0">{username}</p>
@@ -68,11 +74,6 @@ const Menu = () => {
                 alt=""
             />
           </Link>
-        </div>
-        <div className="logoutMenu">
-          <button className="mx-2 btn btn-outline-dark shadow-none" onClick={logoutSession}>
-            <p className="m-0">logout</p>
-          </button>
         </div>
       </div>{" "}
       {/*profilMenu */}
