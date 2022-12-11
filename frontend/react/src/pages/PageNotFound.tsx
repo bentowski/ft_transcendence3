5360613b86e0cfd7bcdcacf6af2423cc736f2f8a
ivloisy <ivloisy@student.42.fr>
2022-12-12 00:04:32 +0100

    Carlo in da place

--- frontend/react/src/pages/PageNotFound.tsx
+++ frontend/react/src/pages/PageNotFound.tsx
@@ -1,10 +1,11 @@
 import { Component } from "react";
+import "../styles/pages/pagenotfound.css"
 
 class PageNotFound extends Component {
     render(): JSX.Element {
         return (
-            <div>
-                PageNotFound
+            <div className="pageNotFound">
+                <h1>Page Not Found</h1>
             </div>
         );
     }
